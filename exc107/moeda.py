16b574eb4accf710346498b27b3237532a921227
Felipe <78973333+Fiwip@users.noreply.github.com>
2024-02-22 09:04:39 -0300

    Add files via upload

---
+++ exc107/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(n=0, p=0):
+    n += n * p/100
+    return n
+
+
+def diminuir(n=0, p=0):
+    n -= n * p/100
+    return n
+
+
+def metade(n):
+    n = n/2
+    return n
+
+
+def dobro(n):
+    n *= 2
+    return n
